99462a1a4f7e374da3cb5e0c9a7a1fa0173d5b9a
Diego <consultoresartificialbrain@gmail.com>
2021-01-27 10:35:28 -0600

    agregando a la master

---
+++ src/models/BannerCategoria.js
@@ -0,0 +1,21 @@
+const {Schema,model} = require('mongoose');
+
+const bannerCategoria = new Schema({
+    estilo: Number,
+    banners: [{
+        orientacion: Number,
+        imagenBanner: String,
+        vincular: Boolean,
+        mostrarProductos: Boolean,
+        mostrarTitulo: Boolean,
+        tipo: {
+            categoria: String,
+            temporada: String
+        }
+    }],
+    publicado: Boolean
+},{
+    timestamps: true
+})
+
+module.exports = model('bannerCategoria',bannerCategoria);
---
+++ src/routes/bannerCategoria.js
@@ -0,0 +1,31 @@
+const { Router } = require('express');
+const router = Router();
+const auth = require('../middleware/auth');
+const { 
+    subirImagen,
+    createBanner,
+    getBanners,
+    agregateBanner,
+    deleteBanner,
+    eliminarImagen,
+    editSubBanner,
+    deleteSubCanner,
+    publishedBanner,
+    getBannersAdmin
+} = require('../controllers/bannerCategoria');
+
+router.route('/')
+    .get(getBanners)
+    .post(auth,createBanner);
+
+router.route('/admin/').get(auth,getBannersAdmin)
+
+router.route('/:idBanner').put(auth,subirImagen,agregateBanner).delete(auth,deleteBanner);
+
+router.route('/:idBanner/action/:idSubBanner').put(auth,subirImagen,editSubBanner).delete(auth,deleteSubCanner);
+
+router.route('/imagen/:idBanner').delete(auth,eliminarImagen);
+
+router.route('/publicar/:idBanner').put(auth,publishedBanner);
+
+module.exports = router;
